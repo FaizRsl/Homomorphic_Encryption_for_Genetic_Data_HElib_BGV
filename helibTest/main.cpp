23bea5a4baefb12486117de1e85fb0f29a325e4d
Faiz <faiz_qwerty@hotmail.com>
2023-09-04 14:11:51 +0800

    "added comparison, sorting, and minmax function"

--- helibTest/main.cpp
+++ helibTest/main.cpp
@@ -1,186 +1,81 @@
 #include <iostream>
+#include <time.h>
+#include <random>
 
 #include <helib/helib.h>
+#include <helib/debugging.h>
+#include <helib/Context.h>
+#include <helib/polyEval.h>
+#include "tools.h"
+#include "comparator.h"
+#include "sorting.cpp"
+#include "compare.cpp"
+#include "minmax.cpp"
+
+using namespace std;
+using namespace NTL;
+using namespace helib;
+using namespace he_cmp;
 
 int main(int argc, char* argv[])
 {
     /*  Example of BGV scheme  */
 
     // Plaintext prime modulus
-    unsigned long p = 81667;
+    //unsigned long p = 81667;
     // Cyclotomic polynomial - defines phi(m)
-    unsigned long m = 32109;
+    //unsigned long m = 32109;
     // Hensel lifting (default = 1)
-    unsigned long r = 1;
+    //unsigned long r = 1;
     // Number of bits of the modulus chain
-    unsigned long bits = 500;
+    //unsigned long bits = 500;
     // Number of columns of Key-Switching matrix (default = 2 or 3)
-    unsigned long c = 2;
+    //unsigned long c = 2;
 
-    std::cout << "Initialising context object..." << std::endl;
-    // Initialize context
-    // This object will hold information about the algebra created from the
-    // previously set parameters
-    helib::Context context = helib::ContextBuilder<helib::BGV>()
-            .m(m)
-            .p(p)
-            .r(r)
-            .bits(bits)
-            .c(c)
-            .build();
+    char *variableList[9];
 
-    // Print the context
-    context.printout();
-    std::cout << std::endl;
-
-    // Print the security level
-    std::cout << "Security: " << context.securityLevel() << std::endl;
-
-    // Secret key management
-    std::cout << "Creating secret key..." << std::endl;
-    // Create a secret key associated with the context
-    helib::SecKey secret_key(context);
-    // Generate the secret key
-    secret_key.GenSecKey();
-    std::cout << "Generating key-switching matrices..." << std::endl;
-    // Compute key-switching matrices that we need
-    helib::addSome1DMatrices(secret_key);
-
-    // Public key management
-    // Set the secret key (upcast: SecKey is a subclass of PubKey)
-    const helib::PubKey& public_key = secret_key;
-
-    // Get the EncryptedArray of the context
-    const helib::EncryptedArray& ea = context.getEA();
-
-    // Get the number of slot (phi(m))
-    long nslots = ea.size();
-    std::cout << "Number of slots: " << nslots << std::endl;
-
-    // Create a vector of long with nslots elements
-    helib::Ptxt<helib::BGV> ptxt(context);
-    // Set it with numbers 0..nslots - 1
-    // ptxt = [0] [1] [2] ... [nslots-2] [nslots-1]
-    int random;
-    for (int i = 0; i < ptxt.size(); ++i) {
-        random = rand() % 10;
-        ptxt[i] = random;
+    for(int i=0; i<sizeof(variableList); i++){
+        variableList[i] = new char[1000];
     }
 
-    // Print the plaintext
-    std::cout << "Initial Plaintext: " << ptxt << std::endl;
+    cout << "Please set arguments to all 8 variables:" << endl;
+    cout << "plaintext modulus (prime number):" << endl;
+    cin >> variableList[0];
+    cout << "dimension of a vector space over slot finite field:" << endl;
+    cin >> variableList[1];
+    cout << "order of cyclotomic ring:" << endl;
+    cin >> variableList[2];
+    cout << "min bitsize of the ciphertext modulus in ciphertext:" << endl;
+    cin >> variableList[3];
+    cout << "length of field vector:" << endl;
+    cin >> variableList[4];
+    cout << "number of values to be sorted:" << endl;
+    cin >> variableList[5];
+    cout << "number of experiments:" << endl;
+    cin >> variableList[6];
+    cout << "type of circuit(B,T,U):" << endl;
+    cin >> variableList[7];
+    cout << "level of consecutive comparison (depth):" << endl;
+    cin >> variableList[8];
 
-    // Create a ciphertext object
-    helib::Ctxt ctxt(public_key);
-    // Encrypt the plaintext using the public_key
-    public_key.Encrypt(ctxt, ptxt);
+    int input;
 
-    /********** Operations **********/
-    // Ciphertext and plaintext operations are performed
-    // "entry-wise".
+    cout << "Choose the function you want to run:" << endl;
+    cout << "1) comparison " << endl;
+    cout << "2) sorting " << endl;
+    cout << "3) minmax " << endl;
+    cin >> input;
 
-    // Square the ciphertext
-    // [0] [1] [2] [3] [4] ... [nslots-1]
-    // -> [0] [1] [4] [9] [16] ... [(nslots-1)*(nslots-1)]
-    ctxt.multiplyBy(ctxt);
-    // Plaintext version
-    //ptxt.multiplyBy(ptxt);
-    //std::cout << "Plaintext Result  for multiplication: " << ptxt << std::endl;
-
-    helib::Ptxt<helib::BGV> test_plaintext_result(context);
-    secret_key.Decrypt(test_plaintext_result, ctxt);
-    std::cout << "Decrypted Result for multiplication: " << test_plaintext_result << std::endl;
-
-    // Divide the ciphertext by itself
-    // To do this we must calculate the multiplicative inverse using Fermat's
-    // Little Theorem.  We calculate a^{-1} = a^{p-2} mod p, where a is non-zero
-    // and p is our plaintext prime.
-    // First make a copy of the ctxt using copy constructor
-    helib::Ctxt ctxt_divisor(ctxt);
-    // Raise the copy to the exponent p-2
-    // [0] [1] [4] ... [16] -> [0] [1] [1] ... [1]
-    // Note: 0 is a special case because 0^n = 0 for any power n
-    ctxt_divisor.power(2);
-    // a^{p-2}*a = a^{-1}*a = a / a = 1;
-    helib::Ptxt<helib::BGV> test2_plaintext_result(context);
-    secret_key.Decrypt(test2_plaintext_result, ctxt_divisor);
-    std::cout << "Decrypted Result for power: " << test2_plaintext_result << std::endl;
-    //ctxt.multiplyBy(ctxt_divisor);
-    ctxt = ctxt_divisor;
-    // Plaintext version
-    //helib::Ptxt<helib::BGV> ptxt_divisor(ptxt);
-    //ptxt_divisor.power(2);
-    //std::cout << "Plaintext Result power: " << ptxt_divisor << std::endl;
-    //ptxt.multiplyBy(ptxt_divisor);
-    //ptxt = ptxt_divisor;
-    // [0] [1] [1] ... [1] [1] -> [0] [2] [2] ... [2] [2]
-    ctxt += ctxt;
-    //ptxt += ptxt;
-
-    helib::Ptxt<helib::BGV> test3_plaintext_result(context);
-    secret_key.Decrypt(test3_plaintext_result, ctxt);
-    std::cout << "Decrypted Result for addition: " << test3_plaintext_result << std::endl;
-    //std::cout << "Plaintext Result for addition: " << ptxt << std::endl;
-
-    // Subtract it from itself (result should be 0)
-    // i.e. [0] [0] [0] [0] ... [0] [0]
-//    ctxt.divideBy2();
-//    helib::Ptxt<helib::BGV> test4_plaintext_result(context);
-//    secret_key.Decrypt(test4_plaintext_result, ctxt);
-//    std::cout << "Decrypted Result for division by 2: " << test4_plaintext_result << std::endl;
-    //ctxt -= ctxt;
-    // Plaintext version
-    //ptxt -= ptxt;
-
-    // Create a plaintext for decryption
-    helib::Ptxt<helib::BGV> plaintext_result(context);
-    // Decrypt the modified ciphertext
-    secret_key.Decrypt(plaintext_result, ctxt);
-
-    //std::cout << "Operation: 2(a*a)/(a*a) - 2(a*a)/(a*a) = 0" << std::endl;
-    // Print the decrypted plaintext
-    // Should be [0] [0] [0] ... [0] [0]
-    //std::cout << "Decrypted Result: " << plaintext_result << std::endl;
-    // Print the plaintext version result, should be the same as the ctxt version
-    //std::cout << "Plaintext Result: " << ptxt << std::endl;
-
-    // We can also add constants
-    // [0] [0] [0] ... [0] [0] -> [1] [1] [1] ... [1] [1]
-    ctxt.addConstant(NTL::ZZX(70l));
-    helib::Ptxt<helib::BGV> test4_plaintext_result(context);
-    secret_key.Decrypt(test4_plaintext_result, ctxt);
-    std::cout << "Decrypted Result for addition of 70 using addConstant: " << test4_plaintext_result << std::endl;
-    // Plaintext version
-    //ptxt.addConstant(NTL::ZZX(1l));
-
-    // And multiply by constants
-    // [1] [1] [1] ... [1] [1]
-    // -> [1*1] [1*1] [1*1] ... [1*1] [1*1] = [1] [1] [1] ... [1] [1]
-    ctxt *= 10l;
-    helib::Ptxt<helib::BGV> test5_plaintext_result(context);
-    secret_key.Decrypt(test5_plaintext_result, ctxt);
-    std::cout << "Decrypted Result for multiplication of 10: " << test5_plaintext_result << std::endl;
-    // Plaintext version
-    //ptxt *= 1l;
-
-    // We can also perform ciphertext-plaintext operations
-    // ctxt = [1] [1] [1] ... [1] [1], ptxt = [1] [1] [1] ... [1] [1]
-    // ctxt + ptxt = [2] [2] [2] ... [2] [2]
-    // Note: the output of this is also a ciphertext
-    ctxt += ptxt;
-    helib::Ptxt<helib::BGV> test6_plaintext_result(context);
-    secret_key.Decrypt(test6_plaintext_result, ctxt);
-    std::cout << "Plaintext Result: " << ptxt << std::endl;
-    std::cout << "Decrypted Result for adding plaintext value: " << test6_plaintext_result << std::endl;
-
-    // Decrypt the modified ciphertext into a new plaintext
-    helib::Ptxt<helib::BGV> new_plaintext_result(context);
-    secret_key.Decrypt(new_plaintext_result, ctxt);
-
-    std::cout << "Operation: Enc{(0 + 1)*1} + (0 + 1)*1" << std::endl;
-    // Print the decrypted plaintext
-    // Should be [2] [2] [2] ... [2] [2]
-    std::cout << "Decrypted Result: " << new_plaintext_result << std::endl;
+    switch(input){
+        case 1:
+            compare_function(variableList);
+        case 2:
+            sorting_function(variableList);
+        case 3:
+            minmax_function(variableList);
+        default:
+            cout << "Invalid choice!" << endl;
+    }
 
     return 0;
 }
--- helibTest/test.cpp
+++ helibTest/test.cpp
@@ -1,220 +1,186 @@
 #include <iostream>
 
 #include <helib/helib.h>
-#include <helib/binaryArith.h>
-#include <helib/intraSlot.h>
 
 int main(int argc, char* argv[])
 {
-    // First set up parameters.
-    //
-    // NOTE: The parameters used in this example code are for demonstration only.
-    // They were chosen to provide the best performance of execution while
-    // providing the context to demonstrate how to use the "Binary Arithmetic
-    // APIs". The parameters do not provide the security level that might be
-    // required by real use/application scenarios.
+    /*  Example of BGV scheme  */
 
-    // Plaintext prime modulus.
-    long p = 2;
-    // Cyclotomic polynomial - defines phi(m).
-    long m = 4095;
-    // Hensel lifting (default = 1).
-    long r = 1;
-    // Number of bits of the modulus chain.
-    long bits = 500;
-    // Number of columns of Key-Switching matrix (typically 2 or 3).
-    long c = 2;
-    // Factorisation of m required for bootstrapping.
-    std::vector<long> mvec = {7, 5, 9, 13};
-    // Generating set of Zm* group.
-    std::vector<long> gens = {2341, 3277, 911};
-    // Orders of the previous generators.
-    std::vector<long> ords = {6, 4, 6};
-
-    std::cout << "\n*********************************************************";
-    std::cout << "\n*            Basic Binary Arithmetic Example            *";
-    std::cout << "\n*            ===============================            *";
-    std::cout << "\n*                                                       *";
-    std::cout << "\n* This is a sample program for education purposes only. *";
-    std::cout << "\n* It attempts to demonstrate the use of the API for the *";
-    std::cout << "\n* binary arithmetic operations that can be performed.   *";
-    std::cout << "\n*                                                       *";
-    std::cout << "\n*********************************************************";
-    std::cout << std::endl;
+    // Plaintext prime modulus
+    unsigned long p = 81667;
+    // Cyclotomic polynomial - defines phi(m)
+    unsigned long m = 32109;
+    // Hensel lifting (default = 1)
+    unsigned long r = 1;
+    // Number of bits of the modulus chain
+    unsigned long bits = 500;
+    // Number of columns of Key-Switching matrix (default = 2 or 3)
+    unsigned long c = 2;
 
     std::cout << "Initialising context object..." << std::endl;
-    // Initialize the context.
+    // Initialize context
     // This object will hold information about the algebra created from the
-    // previously set parameters.
+    // previously set parameters
     helib::Context context = helib::ContextBuilder<helib::BGV>()
             .m(m)
             .p(p)
             .r(r)
-            .gens(gens)
-            .ords(ords)
             .bits(bits)
             .c(c)
-            .bootstrappable(true)
-            .mvec(mvec)
             .build();
 
-    // Print the context.
+    // Print the context
     context.printout();
     std::cout << std::endl;
 
-    // Print the security level.
+    // Print the security level
     std::cout << "Security: " << context.securityLevel() << std::endl;
 
-    // Secret key management.
+    // Secret key management
     std::cout << "Creating secret key..." << std::endl;
-    // Create a secret key associated with the context.
+    // Create a secret key associated with the context
     helib::SecKey secret_key(context);
-    // Generate the secret key.
+    // Generate the secret key
     secret_key.GenSecKey();
+    std::cout << "Generating key-switching matrices..." << std::endl;
+    // Compute key-switching matrices that we need
+    helib::addSome1DMatrices(secret_key);
 
-    // Generate bootstrapping data.
-    secret_key.genRecryptData();
-
-    // Public key management.
-    // Set the secret key (upcast: SecKey is a subclass of PubKey).
+    // Public key management
+    // Set the secret key (upcast: SecKey is a subclass of PubKey)
     const helib::PubKey& public_key = secret_key;
 
-    // Get the EncryptedArray of the context.
+    // Get the EncryptedArray of the context
     const helib::EncryptedArray& ea = context.getEA();
 
-    // Build the unpack slot encoding.
-    std::vector<helib::zzX> unpackSlotEncoding;
-    buildUnpackSlotEncoding(unpackSlotEncoding, ea);
-
-    // Get the number of slot (phi(m)).
+    // Get the number of slot (phi(m))
     long nslots = ea.size();
     std::cout << "Number of slots: " << nslots << std::endl;
 
-    // Generate three random binary numbers a, b, c.
-    // Encrypt them under BGV.
-    // Calculate a * b + c with HElib's binary arithmetic functions, then decrypt
-    // the result.
-    // Next, calculate a + b + c with HElib's binary arithmetic functions, then
-    // decrypt the result.
-    // Finally, calculate popcnt(a) with HElib's binary arithmetic functions,
-    // then decrypt the result.  Note that popcnt, also known as hamming weight
-    // or bit summation, returns the count of non-zero bits.
-
-    // Each bit of the binary number is encoded into a single ciphertext. Thus
-    // for a 16 bit binary number, we will represent this as an array of 16
-    // unique ciphertexts.
-    // i.e. b0 = [0] [0] [0] ... [0] [0] [0]        ciphertext for bit 0
-    //      b1 = [1] [1] [1] ... [1] [1] [1]        ciphertext for bit 1
-    //      b2 = [1] [1] [1] ... [1] [1] [1]        ciphertext for bit 2
-    // These 3 ciphertexts represent the 3-bit binary number 110b = 6
-
-    // Note: several numbers can be encoded across the slots of each ciphertext
-    // which would result in several parallel slot-wise operations.
-    // For simplicity we place the same data into each slot of each ciphertext,
-    // printing out only the back of each vector.
-    // NB: fifteenOrLess4Four max is 15 bits. Later in the code we pop the MSB.
-    long bitSize = 16;
-    long outSize = 2 * bitSize;
-    long a_data = NTL::RandomBits_long(bitSize);
-    long b_data = NTL::RandomBits_long(bitSize);
-    long c_data = NTL::RandomBits_long(bitSize);
-
-    std::cout << "Pre-encryption data:" << std::endl;
-    std::cout << "a = " << a_data << std::endl;
-    std::cout << "b = " << b_data << std::endl;
-    std::cout << "c = " << c_data << std::endl;
-
-    // Use a scratch ciphertext to populate vectors.
-    helib::Ctxt scratch(public_key);
-    std::vector<helib::Ctxt> encrypted_a(bitSize, scratch);
-    std::vector<helib::Ctxt> encrypted_b(bitSize, scratch);
-    std::vector<helib::Ctxt> encrypted_c(bitSize, scratch);
-    // Encrypt the data in binary representation.
-    for (long i = 0; i < bitSize; ++i) {
-        std::vector<long> a_vec(ea.size());
-        std::vector<long> b_vec(ea.size());
-        std::vector<long> c_vec(ea.size());
-        // Extract the i'th bit of a,b,c.
-        for (auto& slot : a_vec)
-            slot = (a_data >> i) & 1;
-        for (auto& slot : b_vec)
-            slot = (b_data >> i) & 1;
-        for (auto& slot : c_vec)
-            slot = (c_data >> i) & 1;
-        ea.encrypt(encrypted_a[i], public_key, a_vec);
-        ea.encrypt(encrypted_b[i], public_key, b_vec);
-        ea.encrypt(encrypted_c[i], public_key, c_vec);
+    // Create a vector of long with nslots elements
+    helib::Ptxt<helib::BGV> ptxt(context);
+    // Set it with numbers 0..nslots - 1
+    // ptxt = [0] [1] [2] ... [nslots-2] [nslots-1]
+    int random;
+    for (int i = 0; i < ptxt.size(); ++i) {
+        random = rand() % 10;
+        ptxt[i] = random;
     }
 
-    // Although in general binary numbers are represented here as
-    // std::vector<helib::Ctxt> the binaryArith APIs for HElib use the PtrVector
-    // wrappers instead, e.g. helib::CtPtrs_vectorCt. These are nothing more than
-    // thin wrapper classes to standardise access to different vector types, such
-    // as NTL::Vec and std::vector. They do not take ownership of the underlying
-    // object but merely provide access to it.
-    //
-    // helib::CtPtrMat_vectorCt is a wrapper for
-    // std::vector<std::vector<helib::Ctxt>>, used for representing a list of
-    // encrypted binary numbers.
+    // Print the plaintext
+    std::cout << "Initial Plaintext: " << ptxt << std::endl;
 
-    // Perform the multiplication first and put it in encrypted_product.
-    std::vector<helib::Ctxt> encrypted_product;
-    helib::CtPtrs_vectorCt product_wrapper(encrypted_product);
-    helib::multTwoNumbers(
-            product_wrapper,
-            helib::CtPtrs_vectorCt(encrypted_a),
-            helib::CtPtrs_vectorCt(encrypted_b),
-            false, // This means the rhs is unsigned rather
-            // than 2's complement.
-            outSize, // Outsize is the limit on the number of bits in the output.
-            &unpackSlotEncoding); // Information needed for bootstrapping.
+    // Create a ciphertext object
+    helib::Ctxt ctxt(public_key);
+    // Encrypt the plaintext using the public_key
+    public_key.Encrypt(ctxt, ptxt);
 
-    // Now perform the encrypted sum and put it in encrypted_result.
-    std::vector<helib::Ctxt> encrypted_result;
-    helib::CtPtrs_vectorCt result_wrapper(encrypted_result);
-    helib::addTwoNumbers(
-            result_wrapper,
-            product_wrapper,
-            helib::CtPtrs_vectorCt(encrypted_c),
-            false, // This means the number are unsigned rather than 2's
-            // complement.
-            &unpackSlotEncoding); // Information needed for bootstrapping.
+    /********** Operations **********/
+    // Ciphertext and plaintext operations are performed
+    // "entry-wise".
 
-    // Decrypt and print the result.
-    std::vector<long> decrypted_result;
-    helib::decryptBinaryNums(decrypted_result, result_wrapper, secret_key, ea);
-    std::cout << "a*b+c = " << decrypted_result.back() << std::endl;
+    // Square the ciphertext
+    // [0] [1] [2] [3] [4] ... [nslots-1]
+    // -> [0] [1] [4] [9] [16] ... [(nslots-1)*(nslots-1)]
+    ctxt.multiplyBy(ctxt);
+    // Plaintext version
+    //ptxt.multiplyBy(ptxt);
+    //std::cout << "Plaintext Result  for multiplication: " << ptxt << std::endl;
 
-    // Now calculate the sum of a, b and c using the addManyNumbers function.
-    encrypted_result.clear();
-    decrypted_result.clear();
-    /*std::vector<std::vector<helib::Ctxt>> summands = {encrypted_a,
-                                                      encrypted_b,
-                                                      encrypted_c};
-    helib::CtPtrs_vectorCt summands_wrapper(summands);
-    helib::addManyNumbers(
-            result_wrapper,
-            summands,
-            0,                    // sizeLimit=0 means use as many bits as needed.
-            &unpackSlotEncoding); // Information needed for bootstrapping.
+    helib::Ptxt<helib::BGV> test_plaintext_result(context);
+    secret_key.Decrypt(test_plaintext_result, ctxt);
+    std::cout << "Decrypted Result for multiplication: " << test_plaintext_result << std::endl;
 
-    // Decrypt and print the result.
-    helib::decryptBinaryNums(decrypted_result, result_wrapper, secret_key, ea);
-    std::cout << "a+b+c = " << decrypted_result.back() << std::endl;*/
+    // Divide the ciphertext by itself
+    // To do this we must calculate the multiplicative inverse using Fermat's
+    // Little Theorem.  We calculate a^{-1} = a^{p-2} mod p, where a is non-zero
+    // and p is our plaintext prime.
+    // First make a copy of the ctxt using copy constructor
+    helib::Ctxt ctxt_divisor(ctxt);
+    // Raise the copy to the exponent p-2
+    // [0] [1] [4] ... [16] -> [0] [1] [1] ... [1]
+    // Note: 0 is a special case because 0^n = 0 for any power n
+    ctxt_divisor.power(2);
+    // a^{p-2}*a = a^{-1}*a = a / a = 1;
+    helib::Ptxt<helib::BGV> test2_plaintext_result(context);
+    secret_key.Decrypt(test2_plaintext_result, ctxt_divisor);
+    std::cout << "Decrypted Result for power: " << test2_plaintext_result << std::endl;
+    //ctxt.multiplyBy(ctxt_divisor);
+    ctxt = ctxt_divisor;
+    // Plaintext version
+    //helib::Ptxt<helib::BGV> ptxt_divisor(ptxt);
+    //ptxt_divisor.power(2);
+    //std::cout << "Plaintext Result power: " << ptxt_divisor << std::endl;
+    //ptxt.multiplyBy(ptxt_divisor);
+    //ptxt = ptxt_divisor;
+    // [0] [1] [1] ... [1] [1] -> [0] [2] [2] ... [2] [2]
+    ctxt += ctxt;
+    //ptxt += ptxt;
 
-    // This section calculates popcnt(a) using the fifteenOrLess4Four
-    // function.
-    // Note: the output i.e. encrypted_result should be of size 4
-    // because 4 bits are required to represent numbers in [0,15].
-    encrypted_result.resize(4lu, scratch);
-    decrypted_result.clear();
-    encrypted_a.pop_back(); // drop the MSB since we only support up to 15 bits.
-    helib::fifteenOrLess4Four(result_wrapper,
-                              helib::CtPtrs_vectorCt(encrypted_a));
+    helib::Ptxt<helib::BGV> test3_plaintext_result(context);
+    secret_key.Decrypt(test3_plaintext_result, ctxt);
+    std::cout << "Decrypted Result for addition: " << test3_plaintext_result << std::endl;
+    //std::cout << "Plaintext Result for addition: " << ptxt << std::endl;
 
-    // Decrypt and print the result.
-    helib::decryptBinaryNums(decrypted_result, result_wrapper, secret_key, ea);
-    std::cout << "popcnt(a) = " << decrypted_result.back() << std::endl;
+    // Subtract it from itself (result should be 0)
+    // i.e. [0] [0] [0] [0] ... [0] [0]
+//    ctxt.divideBy2();
+//    helib::Ptxt<helib::BGV> test4_plaintext_result(context);
+//    secret_key.Decrypt(test4_plaintext_result, ctxt);
+//    std::cout << "Decrypted Result for division by 2: " << test4_plaintext_result << std::endl;
+    //ctxt -= ctxt;
+    // Plaintext version
+    //ptxt -= ptxt;
+
+    // Create a plaintext for decryption
+    helib::Ptxt<helib::BGV> plaintext_result(context);
+    // Decrypt the modified ciphertext
+    secret_key.Decrypt(plaintext_result, ctxt);
+
+    //std::cout << "Operation: 2(a*a)/(a*a) - 2(a*a)/(a*a) = 0" << std::endl;
+    // Print the decrypted plaintext
+    // Should be [0] [0] [0] ... [0] [0]
+    //std::cout << "Decrypted Result: " << plaintext_result << std::endl;
+    // Print the plaintext version result, should be the same as the ctxt version
+    //std::cout << "Plaintext Result: " << ptxt << std::endl;
+
+    // We can also add constants
+    // [0] [0] [0] ... [0] [0] -> [1] [1] [1] ... [1] [1]
+    ctxt.addConstant(NTL::ZZX(70l));
+    helib::Ptxt<helib::BGV> test4_plaintext_result(context);
+    secret_key.Decrypt(test4_plaintext_result, ctxt);
+    std::cout << "Decrypted Result for addition of 70 using addConstant: " << test4_plaintext_result << std::endl;
+    // Plaintext version
+    //ptxt.addConstant(NTL::ZZX(1l));
+
+    // And multiply by constants
+    // [1] [1] [1] ... [1] [1]
+    // -> [1*1] [1*1] [1*1] ... [1*1] [1*1] = [1] [1] [1] ... [1] [1]
+    ctxt *= 10l;
+    helib::Ptxt<helib::BGV> test5_plaintext_result(context);
+    secret_key.Decrypt(test5_plaintext_result, ctxt);
+    std::cout << "Decrypted Result for multiplication of 10: " << test5_plaintext_result << std::endl;
+    // Plaintext version
+    //ptxt *= 1l;
+
+    // We can also perform ciphertext-plaintext operations
+    // ctxt = [1] [1] [1] ... [1] [1], ptxt = [1] [1] [1] ... [1] [1]
+    // ctxt + ptxt = [2] [2] [2] ... [2] [2]
+    // Note: the output of this is also a ciphertext
+    ctxt += ptxt;
+    helib::Ptxt<helib::BGV> test6_plaintext_result(context);
+    secret_key.Decrypt(test6_plaintext_result, ctxt);
+    std::cout << "Plaintext Result: " << ptxt << std::endl;
+    std::cout << "Decrypted Result for adding plaintext value: " << test6_plaintext_result << std::endl;
+
+    // Decrypt the modified ciphertext into a new plaintext
+    helib::Ptxt<helib::BGV> new_plaintext_result(context);
+    secret_key.Decrypt(new_plaintext_result, ctxt);
+
+    std::cout << "Operation: Enc{(0 + 1)*1} + (0 + 1)*1" << std::endl;
+    // Print the decrypted plaintext
+    // Should be [2] [2] [2] ... [2] [2]
+    std::cout << "Decrypted Result: " << new_plaintext_result << std::endl;
 
     return 0;
 }
