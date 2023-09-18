d5c230311c152a634206e585879e21f0b7f47796
faiz007 <faiz007@ubuntuvm.myguest.virtualbox.org>
2023-09-18 18:04:15 +0800

    library removed

--- HElib/build/dependencies/Build/helib_external/CMakeFiles/helib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for helib.
-# This may be replaced when dependencies are built.
--- HElib/build/dependencies/CMakeFiles/dependencies_built.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/dependencies_built.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
